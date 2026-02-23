b3aeaf04e121493229e360d09bbf35a104f4c1a4
ashu1259 <abhishek.auburnsolutions@gmail.com>
2026-02-23 10:21:41 +0530

    ecs.tf modification

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -51,20 +51,17 @@ resource "aws_iam_role_policy_attachment" "ecs_task_execution_attach" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
-# ECS Task Definition for Flask
 resource "aws_ecs_task_definition" "flask_task" {
   family                   = "flask-task"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   cpu                      = "256"
   memory                   = "512"
-  execution_role_arn       = aws_iam_role.ecs_task_execution.arn
 
   container_definitions = jsonencode([
     {
-      name      = "flask"
-      image     = "<flask-ecr-repo-url>:latest"
-      essential = true
+      name  = "flask"
+      image = "<REPLACE_WITH_FLASK_ECR_URI>"
       portMappings = [
         {
           containerPort = 5000
