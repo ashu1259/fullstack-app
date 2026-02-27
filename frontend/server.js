5429ecbf7ffbe70856f8da5281693175817d52e9
ashu1259 <abhishek.auburnsolutions@gmail.com>
2026-02-27 22:00:19 +0530

    backend url change

--- frontend/server.js
+++ frontend/server.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const axios = require("axios");
+const bodyParser = require("body-parser");
 
 const app = express();
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Docker service name
+// VERY IMPORTANT: Use docker-compose service name
 const BACKEND_URL = "http://backend:5000/process";
 
 app.get("/", (req, res) => {
@@ -14,14 +14,17 @@ app.get("/", (req, res) => {
 });
 
 app.post("/submit", async (req, res) => {
+  const { name, email } = req.body;
+
   try {
     const response = await axios.post(BACKEND_URL, {
-      name: req.body.name,
-      email: req.body.email,
+      name,
+      email,
     });
 
     res.render("index", { response: response.data.message });
   } catch (error) {
+    console.error("Backend Error:", error.message);
     res.render("index", { response: "Error connecting to backend" });
   }
 });
