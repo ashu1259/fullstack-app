9443538966c92833972aba54481e6899ce9b4d9c
ashu1259 <abhishek.auburnsolutions@gmail.com>
2026-02-27 21:46:38 +0530

    cicd initial

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+import os
 
 app = Flask(__name__)
 
@@ -11,5 +12,11 @@ def process():
     message = f"Received Name: {name}, Email: {email}"
     return jsonify({"message": message})
 
+@app.route("/health")
+def health():
+    return jsonify({"status": "Backend Running"})
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
--- frontend/server.js
+++ frontend/server.js
@@ -1,23 +1,24 @@
 const express = require("express");
-const axios = require("axios");
 const bodyParser = require("body-parser");
+const axios = require("axios");
 
 const app = express();
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// Docker service name
+const BACKEND_URL = "http://backend:5000/process";
 
 app.get("/", (req, res) => {
   res.render("index", { response: null });
 });
 
 app.post("/submit", async (req, res) => {
-  const { name, email } = req.body;
-
   try {
-    const response = await axios.post(
-  "http://flask-backend-service:5000/process",
-  { name, email }
-);
+    const response = await axios.post(BACKEND_URL, {
+      name: req.body.name,
+      email: req.body.email,
+    });
 
     res.render("index", { response: response.data.message });
   } catch (error) {
@@ -25,6 +26,6 @@ app.post("/submit", async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
+app.listen(3000, "0.0.0.0", () => {
   console.log("Frontend running on port 3000");
 });
